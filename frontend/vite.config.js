59d9821e2624b30b43b9973e16a1ef4078775cc5
coepelib-spec <coep.elib@gmail.com>
2025-12-26 17:59:58 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Explicitly defining the root helps Vercel during Monorepo builds
+  root: './',
+  build: {
+    outDir: 'dist',
+  },
+});
 
